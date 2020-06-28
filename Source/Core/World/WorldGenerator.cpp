1df34724667bceca7e5e1c776796d122e061b379
Samuel Rasquinha <samuelrasquinha@gmail.com>
2020-06-28 19:10:19 +0530

    Updated world gen

--- Source/Core/World/WorldGenerator.cpp
+++ Source/Core/World/WorldGenerator.cpp
@@ -2,7 +2,7 @@
 
 namespace Minecraft
 {
-    void SetVerticalBlocks(Chunk* chunk, BlockType type, int x, int z, int y_level)
+    void SetVerticalBlocks(Chunk* chunk, int x, int z, int y_level)
     {
         for (int i = 0; i < y_level; i++)
         {
@@ -24,35 +24,13 @@ namespace Minecraft
         myNoise.SetNoiseType(FastNoise::PerlinFractal);
 
         glm::vec3 position;
-        float generated_pos[3];
-
-        float generated_x = 0;
-        float generated_y = 0;
-        float generated_z = 0;
-
-        float HeightMap[16][16]; // 2D heightmap to create terrain
+        float noiseScale = 20.0f;
 
         for (int x = 0; x < ChunkSizeX; x++)
         {
-            for (int y = 0; y < ChunkSizeY; y++)
+            for (int z = 0; z < ChunkSizeZ; z++)
             {
-                HeightMap[x][y] = myNoise.GetNoise(x + chunk->p_Position.x * ChunkSizeX, y + chunk->p_Position.z * ChunkSizeZ);
-            }
-        }
-
-
-        for (int x = 0; x < ChunkSizeX; x++)
-        {
-            for (int y = 0; y < ChunkSizeY; y++)
-            {
-                for (int z = 0; z < ChunkSizeZ; z++)
-                {
-                    generated_x = x;
-                    generated_y = (HeightMap[x][z] / 2 + 1) * ChunkSizeY;
-                    generated_z = z;
-
-                    SetVerticalBlocks(chunk, BlockType::Dirt, generated_x, generated_z, generated_y);
-                }
+                SetVerticalBlocks(chunk, x, z, (myNoise.GetNoise(x, z) / 2 + 1) * noiseScale);
             }
         }
     }
